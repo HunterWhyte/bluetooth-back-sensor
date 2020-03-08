1dec3bb01a5d18135313d4f68d90c91f96544092
Hunter Whyte <61810551+HunterWhyte@users.noreply.github.com>
2020-03-08 11:05:03 -0600

    Add files via upload

---
+++ analog_sensor.ino
@@ -0,0 +1,31 @@
+
+const int analogInPin = A9;  // Analog input pin that the potentiometer is attached to
+int sensorValue = 0;        // value read from the pot
+int led = 7;
+ 
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+ pinMode(9, INPUT_PULLUP); 
+ pinMode(led, OUTPUT); 
+
+ 
+}
+ 
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin); 
+  Serial.print("sensor = " );                       
+  Serial.println(sensorValue);      
+
+  if (sensorValue > 275)
+  {
+    digitalWrite(led, HIGH);
+    delay(500);                 
+  }
+  if (sensorValue < 275)
+  {
+    digitalWrite(led, LOW);
+    delay(500);
+  }
+}
